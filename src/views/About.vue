221f9f2ebe45cf5fcfdab8e3e3fc48180042625d
JUHEEBAEK <gogumangoguma@gmail.com>
2022-02-27 23:24:25 +0900

    [feat] 영어 json 파일읽어서 뿌려주는 방식으로 변경

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="page__title">{{ datas.title }}</h1>
+    <div
+      class="paragraph__container"
+      v-for="(section, index) in datas.contents"
+      :key="`${index}_${section.subTitle}`"
+    >
+      <div class="text__title">{{ section.subTitle }}</div>
+      <div class="box__content">
+        <span v-for="sentense in section.content" :key="sentense">
+          {{ sentense }}
+        </span>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import data from "../assets/Data/en/HowDoesTheInternetWork.json";
+
+export default {
+  name: "AboutTest",
+
+  data: () => ({
+    datas: data,
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+.page__title {
+  padding: 12px;
+}
+.paragraph__container {
+  .text__title {
+    padding: 12px;
+    font-size: 24px;
+    color: grey;
+  }
+  .box__content {
+    padding: 4px 12px;
+    display: flex;
+    flex-direction: column;
+    font-size: 16px;
+  }
+}
+</style>
