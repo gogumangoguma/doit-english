da01169a6bab27b7df4f71c6f7f5cedc2bfd6805
JUHEEBAEK <gogumangoguma@gmail.com>
2022-02-27 23:54:48 +0900

    문장 span으로 쪼개기

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,30 +1,59 @@
 <template>
   <div class="about">
-    <h1 class="page__title">{{ datas.title }}</h1>
+    <h1 class="page__title">{{ dataEn.title }}</h1>
     <div
       class="paragraph__container"
-      v-for="(section, index) in datas.contents"
+      v-for="(section, index) in dataEn.contents"
       :key="`${index}_${section.subTitle}`"
     >
       <div class="text__title">{{ section.subTitle }}</div>
       <div class="box__content">
-        <span v-for="sentense in section.content" :key="sentense">
-          {{ sentense }}
-        </span>
+        <template v-for="(sentense, idx) in section.content">
+          <div :key="`en_sentense_${idx}`" class="sentense__en">
+            <span
+              v-for="(word, i) in splitSentense(sentense)"
+              :key="`word_${i}`"
+            >
+              {{ word }}
+            </span>
+          </div>
+          <div :key="`ko_sentense_${idx}`" class="sentense__ko">
+            <span
+              v-for="(word, i) in splitSentenseKorea(index, idx)"
+              :key="`word_ko_${i}`"
+            >
+              {{ word }}
+            </span>
+          </div>
+        </template>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import data from "../assets/Data/en/HowDoesTheInternetWork.json";
+import dataEn from "../assets/Data/en/HowDoesTheInternetWork.json";
+import dataKo from "../assets/Data/ko/HowDoesTheInternetWork.json";
 
 export default {
   name: "AboutTest",
 
   data: () => ({
-    datas: data,
+    dataEn,
+    dataKo,
   }),
+  methods: {
+    splitSentense(sentense) {
+      return sentense.split(" ");
+    },
+    splitSentenseKorea(index, idx) {
+      const sentense = this.dataKo.contents?.[index]?.content?.[idx];
+      if (sentense !== undefined) {
+        return sentense.split(" ");
+      }
+      return [];
+    },
+  },
 };
 </script>
 
@@ -43,6 +72,14 @@ export default {
     display: flex;
     flex-direction: column;
     font-size: 16px;
+    .sentense__en {
+      font-size: 16px;
+      font-weight: 500;
+    }
+    .sentense__ko {
+      font-weight: 400;
+      color: darkgrey;
+    }
   }
 }
 </style>
